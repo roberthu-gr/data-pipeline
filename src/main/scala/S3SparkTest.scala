4d4b8857e7a10c2851cf2fb30380331241b971db
roberthu-gr <grdevelopment\roberth@a-ord7gb2nefos.gr.development>
2020-01-31 14:23:27 +0000

    REFDATAWRK-154 added job params

--- src/main/scala/S3SparkTest.scala
+++ src/main/scala/S3SparkTest.scala
@@ -1,6 +1,6 @@
 import co.uk.gresearch.aws.glue.Util
 import com.amazonaws.services.glue.{DynamicFrame, GlueContext}
-import com.amazonaws.services.glue.util.JsonOptions
+import com.amazonaws.services.glue.util.{GlueArgParser, JsonOptions}
 import org.apache.spark.SparkContext
 
 object S3SparkTest {
@@ -10,11 +10,30 @@ object S3SparkTest {
     val sc: SparkContext = new SparkContext()
     val glueContext: GlueContext = new GlueContext(sc)
 
-    val inputPaths = Set("s3://refdata-default-refdata-in-file-uploads")
+    val infilesBucket = "s3://refdata-default-refdata-in-file-uploads"
+    val outputBucket = "s3://refdata-default-refdata-in-file-uploads-datalake"
+
+
+
+    val argsWithIdx = args.zipWithIndex
+    val filenameOpt = argsWithIdx.find( _._1 == "ROBTEST").map{case (_,idx) => argsWithIdx(idx+1)._1}
+    val (infilePath,outputPath) = filenameOpt match{
+      case Some(filename) =>
+        println(s"found filename arg: $filename")
+        (s"$infilesBucket/$filename",s"$outputBucket/${filename.split("\\.")(0)}")
+      case None =>
+            println("Missing filename arg")
+          throw new IllegalStateException("Missing mandaroty JOB argument")
+    }
+
+    val inputPaths = Set(infilePath)
     val source = glueContext.getSourceWithFormat("s3", JsonOptions(Map("paths" -> inputPaths)),format = "csv")
     val df: DynamicFrame = source.getDynamicFrame()
     Util.help(df)
     df.show(1)
+
+    val output = glueContext.getSinkWithFormat("s3",JsonOptions(Map("path" -> outputPath)),format = "parquet")
+    output.writeDynamicFrame(df)
   }
 }
 
---
+++ src/main/scala/TestIt.scala
@@ -0,0 +1,6 @@
+object TestIt {
+  def main(args: Array[String]): Unit = {
+    val test = "Test1.csv"
+    println(test.split("\\.")(0))
+  }
+}
